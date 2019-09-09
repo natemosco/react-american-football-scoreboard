9db4e82ab23fdac205c1998059a279e65c0d26aa
Nathaniel Mosco <natemosco@gmail.com>
2019-09-09 19:43:58 -0400

    logic here doesn't work the way you expect. Tried initially declaring a variable = false and then within the event handler reading if false add 3pts for a fieldgoal else add 1 and then set variable to false THISDOES NOT WORK because asynchronous code sets the variable to false every time the page re-renders! SO! the answer was to create a new promise that passes the resolve to .then( and adds the hook value <ie:the score>) So now there is a window.prompt with a message that is tied to a ternary operator to determine the value of a touchdown

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore]= useState(0);
   const [awayScore, setAwayScore]= useState(0);
-
+  const [homeTouchdown, setHomeTouchdown]= useState(false);
+  let touchdownAwayTeam= false;
   return (
     <div className="container">
       <section className="scoreboard">
@@ -30,14 +31,36 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={()=> setHomeScore(homeScore + 6)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={()=> setHomeScore(homeScore + 3)}>Home Field Goal</button>
-          <button className="homeButtons__twoPointConversion" onClick={()=> setHomeScore(homeScore + 2)}>2 Point Conversion</button>
+          <button className="homeButtons__touchdown" onClick={()=> {
+            let mySecondPromise = new Promise((resolve, reject) => {
+              resolve(window.confirm("Did the Home Team Score a field goal?"));
+            })
+            mySecondPromise.then((resOfWindowConfirm) =>  (resOfWindowConfirm)? setHomeScore(homeScore + 7) : setHomeScore(homeScore + 6)) 
+          }}>
+            Home Touchdown
+          </button>
+          <button className="homeButtons__fieldGoal" onClick={()=> setHomeScore(homeScore + 3)}>
+            Home Field Goal
+          </button>
+          <button className="homeButtons__twoPointConversion" onClick={()=> setHomeScore(homeScore + 2)}>
+            2 Point Conversion
+          </button>
         </div>
         <div className="awayButtons">
-            <button className="awayButtons__touchdown" onClick={()=> setAwayScore(awayScore + 6)}>Home Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={()=> setAwayScore(awayScore + 3)}>Home Field Goal</button>
-          <button className="awayButtons__twoPointConversion" onClick={()=> setAwayScore(awayScore + 2)}>2 Point Conversion</button>
+          <button className="awayButtons__touchdown" onClick={()=> {
+            let myFirstPromise = new Promise((resolve, reject) => {
+              resolve(window.confirm("Did the Away Team Score a field goal?"));
+            })
+            myFirstPromise.then((resOfWindowConfirm) =>  (resOfWindowConfirm)? setAwayScore(awayScore + 7) : setAwayScore(awayScore + 6)) 
+          }}>
+            Away Touchdown
+          </button>
+          <button className="awayButtons__fieldGoal" onClick={()=> {setAwayScore(awayScore + 3)}}>
+            Away Field Goal
+          </button>
+          <button className="awayButtons__twoPointConversion" onClick={()=> setAwayScore(awayScore + 2)}>
+            2 Point Conversion
+          </button>
         </div>
       </section>
     </div>
