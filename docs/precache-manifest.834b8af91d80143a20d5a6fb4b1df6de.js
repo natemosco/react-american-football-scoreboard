26816a24aaf1ab18b84e82f0db45c44726c01372
Nathaniel Mosco <natemosco@gmail.com>
2019-09-12 21:46:33 -0400

    yarn build => rename build to docs

---
+++ docs/precache-manifest.834b8af91d80143a20d5a6fb4b1df6de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc8446385dcc4c79baef8894f97db16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d56a993633d8072d4894",
+    "url": "/static/css/main.31c3ebaa.chunk.css"
+  },
+  {
+    "revision": "dcdf05b55f333318bf51",
+    "url": "/static/js/2.62c45116.chunk.js"
+  },
+  {
+    "revision": "d56a993633d8072d4894",
+    "url": "/static/js/main.a19e7527.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1abd8c1a58598974d8415573764ba76a",
+    "url": "/static/media/Ozone.1abd8c1a.ttf"
+  }
+]);
